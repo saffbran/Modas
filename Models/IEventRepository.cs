407ef4a98cd69338d691967f706c2764b22b39da
Brandon Saffert <38892556+saffbran@users.noreply.github.com>
2019-02-18 17:52:06 -0600

    Add files via upload

---
+++ Models/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+namespace Modas.Models
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> Events { get; }
+        IQueryable<Location> Locations { get; }
+
+        Event AddEvent(Event evt);
+        Event UpdateEvent(Event evt);
+        void DeleteEvent(int eventId);
+    }
+}
